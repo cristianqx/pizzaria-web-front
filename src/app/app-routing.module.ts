43fb55b80d43d181d27c27b4f684a4b09ba86d57
Cristian <cristianqx@gmail.com>
2019-09-03 10:40:14 -0300

    Menu do usuário att

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,47 +10,21 @@ const routes: Routes = [
   {path: '', data: { title: 'Dashboard'},
     children: [
       {path: '',component: HomeComponent, canActivate: [AuthGuardService]},
-  /*    {path: 'financeiro', loadChildren: './+financeiro/financeiro.module#FinanceiroModule',data: {
-        title: 'Financeiro'}, canActivate: [FinanceGuardService]},
-     {path: 'accordion', loadChildren: './+accordion/accordion.module#AccordionModule',data: {
-          title: 'Financeiro'}, canActivate: [FinanceGuardService]},
-      {path: 'alert',loadChildren: './+alert/alert.module#AlertModule', data: {
-          title: 'Mensagens'}},
-      
       {path: 'cadastros', data: {title: 'Cadastros'},
         children: [
           {path: 'usuarios',loadChildren: './+cadastros/usuarios/usuarios.module#UsuariosModule',data: {
             title: 'Usuários'}},
         ]
       },   
-/*
+
+
       {path: 'layout', data: {title: 'Cadastros'},
           children: [
             {path: 'configuration',loadChildren: './+layout/configuration/configuration.module#ConfigurationModule',data: {
                 title: 'Usuários'}},
-            {path: 'custom',loadChildren: './+layout/custom/custom.module#CustomModule',data: {
-                title: 'Aeroportos'}},
-            {path: 'content',loadChildren: './+layout/content/content.module#ContentModule', data: {
-                title: 'Parâmetros'}},
-            {path: 'header',loadChildren: './+layout/header/header.module#HeaderModule', data: {
-                title: 'Monetário'}},
-            {path: 'sidebar-left',loadChildren: './+layout/sidebar-left/sidebar-left.module#SidebarLeftModule',data: {
-                title: 'Ponto de Coleta'}},
-            {path: 'sidebar-right',loadChildren: './+layout/sidebar-right/sidebar-right.module#SidebarRightModule',data: {
-                title: 'Gestão de Estrelas'}}
           ]
       },
-      {path: 'boxs',data: {title: 'Coleta'},
-          children: [
-            {path: 'box',loadChildren: './+boxs/box-default/box-default.module#BoxDefaultModule',data: {
-                title: 'Receber Encomenda'}}, 
-            {path: 'info-box',loadChildren: './+boxs/box-info/box-info.module#BoxInfoModule',data: {
-                title: 'Entregar Encomenda'}}, 
-            {path: 'small-box',loadChildren: './+boxs/box-small/box-small.module#BoxSmallModule',data: {
-                title: 'Problemas e Soluções'}}
-          ]
-      },
-      */
+      
       {path: 'dropdown',loadChildren: './+dropdown/dropdown.module#DropdownModule',data: {
             title: 'Dropdown'}},
       {path: 'tabs',loadChildren: './+tabs/tabs.module#TabsModule',data: {
--- src/app/security/profiles/admin-profile.ts
+++ src/app/security/profiles/admin-profile.ts
@@ -8,22 +8,10 @@ export const ADMIN_PROFILE_MENU = {
       {label: 'Cadastros', iconClasses: 'fa fa-pencil-square-o',
         children: [
           {label: 'Usuários', route: 'cadastros/usuarios', iconClasses: 'fa fa-users'},
-          {label: 'Aeroportos', route: 'cadastros/aeroportos', iconClasses: 'fa fa-plane'},
-          {label: 'Monetário', route: 'cadastros/monetario', iconClasses: 'fa fa-usd'},
-          {label: 'Ponto de Coleta', route: 'cadastros/ponto-coleta', iconClasses: 'fa fa-cubes'},
-          {label: 'Gestão de Estrelas', route: 'cadastros/estrelas', iconClasses: 'fa fa-star'},
-          {label: 'Parâmetros', route: 'cadastros/parametros', iconClasses: 'fa fa-database'}
+          {label: 'Produtos', route: 'cadastros/produtos', iconClasses: 'fa fa-cutlery'}
         ]
       },
-      {label: 'Mensagens', route: 'alert', iconClasses: 'fa fa-envelope'},
       {label: 'Relatórios', route: 'dropdown', iconClasses: 'fa fa-line-chart'},
-      {label: 'Coleta', iconClasses: 'fa fa-exchange', 
-          children: [
-            {label: 'Receber Encomenda', route: 'boxs/box'},
-            {label: 'Entregar Encomenda', route: 'boxs/info-box'},
-            {label: 'Problemas e Soluções', route: 'boxs/small-box'}
-          ]
-      }
     ]
   };
   
